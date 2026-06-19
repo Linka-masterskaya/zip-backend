bf287122a7f9d6695fe7210cf8d43f0ec34b3b64
Ренат Седловский <rensedlovskij911@gmail.com>
2026-06-19 21:54:02 +0500

    [AB-7] modify handler with ctx

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,14 +2,33 @@
 package logger
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/Linka-masterskaya/zip-backend/internal/middleware"
 )
 
 const (
 	envDev  = "dev"
 	envProd = "prod"
 )
+
+type ContextHandler struct {
+	slog.Handler // встраиваем — Enabled/WithAttrs/WithGroup наследуются как есть
+}
+
+// Handle вызывается на каждую запись лога. Именно сюда приходит ctx,
+// переданный через logger.InfoContext(ctx, ...).
+func (h ContextHandler) Handle(ctx context.Context, r slog.Record) error {
+	reqId := middleware.GetRequestID(ctx) // будет доступно после слияния AB-17
+
+	if reqId != nil {
+		r.AddAttrs(slog.String("requestID", reqId.(string)))
+	}
+
+	return h.Handler.Handle(ctx, r)
+}
 
 // Init инициализирует и устанавливает дефолтным логгер с настройками,
 // зависищями от оркужения.
@@ -27,5 +46,5 @@ func Init(env string) {
 		})
 	}
 
-	slog.SetDefault(slog.New(handler))
+	slog.SetDefault(slog.New(ContextHandler{handler}))
 }
