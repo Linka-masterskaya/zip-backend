8f00fc1066a366be3db523fa47ff29acc30b459b
Sergey Kolomiichuk <19serega89@mail.ru>
2026-08-06 15:25:34 +0300

    [AB-51] fix: add vary with origin

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -19,6 +19,7 @@ func CORSMiddleware(allowedOrigins []string) func(http.Handler) http.Handler {
 			if _, ok := origins[origin]; ok {
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 				w.Header().Set("Access-Control-Allow-Credentials", "true")
+				w.Header().Add("Vary", "Origin")
 			}
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Request-Id")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
