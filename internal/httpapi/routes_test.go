67c1c23f9f0a1a13221f4671af934b01ef856077
Ivan M. Volosov <eone1712@yandex.ru>
2026-09-04 01:01:24 +0400

    test(httpapi): add new category route

--- internal/httpapi/routes_test.go
+++ internal/httpapi/routes_test.go
@@ -111,6 +111,7 @@ func TestRegisterPictureBankRoutesPatterns(t *testing.T) {
 		"GET /api/v1/pictures/search",
 		"GET /api/v1/pictures/{id}/content",
 		"POST /api/v1/pictures/{id}/import",
+		"GET /api/v1/pictures/category/{categoryId}/list",
 	})
 }
 
